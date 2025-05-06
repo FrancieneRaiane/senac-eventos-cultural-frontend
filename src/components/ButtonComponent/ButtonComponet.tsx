f4e951e2e0292c80d23fc440c85e630e61e58a57
FrancieneRaiane <franciene.sjdr@gmail.com>
2025-05-05 21:50:29 -0300

    primeiro commit

---
+++ src/components/ButtonComponent/ButtonComponet.tsx
@@ -0,0 +1,6 @@
+function ButtonComponent(){
+    return(
+        <button>clique aqui</button>
+    )
+}
+export default ButtonComponent
---
+++ src/pages/EventsPage/EventsPage.tsx
@@ -0,0 +1,13 @@
+import ButtonComponent from '../../components/ButtonComponent/ButtonComponet'
+import styles from './EventsPage.module.css'
+function EventsPage (){
+    return(
+        <>
+            <h1 className={styles.title}> pagina de eventos</h1>
+            <ButtonComponent />
+        </>
+        
+    )
+}
+
+export default EventsPage
