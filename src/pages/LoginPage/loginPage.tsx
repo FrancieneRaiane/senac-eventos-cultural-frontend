8493d9b09c9b16089503a0912aeed9da107bcd24
FrancieneRaiane <franciene.sjdr@gmail.com>
2025-05-09 21:48:57 -0300

    login

--- src/pages/LoginPage/loginPage.tsx
+++ src/pages/LoginPage/loginPage.tsx
@@ -1,21 +1,75 @@
-import React from "react";
+import React, { useState, type FormEvent } from "react";
 import styles from "./LoginPage.module.css";
 
-const LoginPage = () => {
+
+interface LoginProps{
+  onLoginSuccess?: (token: string) => void;
+}
+                                                            // 
+const LoginPage: React.FC<LoginProps> =({ onLoginSuccess})  => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null) ;// tipo string ou null e começa com null 
+  
+  const handleSubmit = async (e: FormEvent) =>{
+    e.preventDefault();
+    setError(null);
+    try{
+      //chamada da API
+      const res = await fetch(
+        "https://senac-eventos-cultural-backend-production.up.railway.app/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password}),
+        }
+      );
+      // se vier res negativa
+      if (!res.ok) {
+        const data = await res.json();
+        throw new Error(data.message || "falha no login");
+      }
+      const data = await res.json();// dado enviado a api 
+      onLoginSuccess?.(data.token);    //retorna um token
+      alert("Login Efetuado"); 
+
+    } catch (err: unknown){
+      if (err instanceof Error){
+        setError(err.message);
+        alert(`Erro ao logar: ${err.message}`);
+      }else{
+        const errorMsg = String(err);
+        setError(errorMsg);
+        alert(`Erro ao logar: ${errorMsg}`);
+      }
+
+    }
+
+  };
+
+
+                                                   ///////////
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <div><h2>SENAC Eventos culturais</h2></div>
+        <div><h2>SENAC Eventos Culturais</h2></div>
         <button className={styles.loginButton}>Login</button>
       </header>
 
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+
+        {error && <div>{error}</div>}
+
         <label htmlFor="email" className={styles.label}>
           Email
         </label>
+
         <input
           type="email"
           id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          //os quatro acima devem ser obrigatorios por conta do banco de dados
           placeholder="Digite seu email"
           required
           className={styles.input}
@@ -26,7 +80,9 @@ const LoginPage = () => {
         </label>
         <input
           type="password"
-          id="senha"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Digite sua senha"
           required
           className={styles.input}
